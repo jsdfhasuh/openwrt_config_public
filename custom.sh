1082326d02448d894f3ae028045b95a7eeb0a943
Administrator <c18122583370@gmail.com>
2025-01-24 13:11:16 +0800

    fixbug

--- custom.sh
+++ custom.sh
@@ -4,10 +4,11 @@ echo "Architecture: $architecture"
 build_root=$2
 # openwrt_files
 openwrt_files=$build_root/openwrt_files
-mkdir -p $openwrt_files
+mkdir -p "$openwrt_files"
 # download clash core
 clash_core_url="https://raw.githubusercontent.com/vernesong/OpenClash/blob/core/master/meta/clash-linux-$architecture.tar.gz"
 clash_core_dir=$openwrt_files/etc/openclash/core
-mkdir -p $clash_core_dir
+echo "Clash core dir: $clash_core_dir"
+mkdir -p "$clash_core_dir"
 echo "Downloading clash core from $clash_core_url"
 curl -L -o $clash_core_dir/clash_meta $clash_core_url
